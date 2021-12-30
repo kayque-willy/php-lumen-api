978df1fb0b45943938ce5eb6651a0cbec49a8b82
opina-ai <kayque-willy@hotmail.com>
2021-12-30 11:47:47 -0300

    Adição de comentário

--- app/Services/Author/AuthorService.php
+++ app/Services/Author/AuthorService.php
@@ -6,12 +6,14 @@ use App\Services\AbstractService;
 
 class AuthorService extends AbstractService
 {
+    // Encriptação da senha
     public function create(array $data): array
     {
         $data['password'] = encrypt($data['password']);
         return $this->repository->create($data);
     }
 
+    // Encriptação da senha
     public function editBy(string $param, array $data): bool
     {
         $data['password'] = encrypt($data['password']);
